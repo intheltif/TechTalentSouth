b2fd51f25bf4b21d08bb592d6efb0734ade4a3c5
Owner <owner@owners-macbook-pro.local>
2020-07-15 23:57:02 -0400

    Created new project for "The Night Porter"

---
+++ swift_ios/iOS-11-Fundamentals/The Night Porter/The Night Porter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Owner on 7/15/20.
+//  Copyright © 2020 Evert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
