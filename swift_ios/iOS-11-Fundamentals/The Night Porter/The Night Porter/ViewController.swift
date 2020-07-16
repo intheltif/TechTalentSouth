841a35f435c8ac2f4737d5b953e288e4e033909d
Owner <owner@owners-macbook-pro.local>
2020-07-16 00:34:39 -0400

    Added some components while following along with the PluralSight video.

--- swift_ios/iOS-11-Fundamentals/The Night Porter/The Night Porter/ViewController.swift
+++ swift_ios/iOS-11-Fundamentals/The Night Porter/The Night Porter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func enableDarkMode(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
